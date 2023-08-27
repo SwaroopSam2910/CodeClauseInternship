80a39caa80e4621f992f40754933ead5014746ed
SWAROOP SAMANTRAY <128119728+SwaroopSam2910@users.noreply.github.com>
2023-08-27 20:30:55 +0530

    Add files via upload

---
+++ Task2 basic online resume/animText.js
@@ -0,0 +1,7 @@
+var typingEffect = new Typed(".span-2",{
+    strings : ["Web Developer.","Professional Coder.","Student."],
+    loop : true,
+    typeSpeed : 23,
+    backSpeed : 17,
+    backDelay : 1500
+  })
